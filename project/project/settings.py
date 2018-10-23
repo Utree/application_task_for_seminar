24cfdce06ee7e1429f0c5f55b2de205c440873ff
Utree <yuukis322+cloud9@gmail.com>
2018-10-23 04:30:20 +0000

    fix

--- project/project/settings.py
+++ project/project/settings.py
@@ -148,26 +148,3 @@ PASSWORD_HASHERS = [
 
 # PWA
 PWA_SERVICE_WORKER_PATH = os.path.join(BASE_DIR, 'static/seminar/js', 'sw.js')
-
-
-# addtional setting for heroku
-# Parse database configuration from $DATABASE_URL
-import dj_database_url
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
-
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-# Allow all host headers
-ALLOWED_HOSTS = ['*']
-
-# Static asset configuration
-import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-  os.path.join(BASE_DIR, 'static'),
-)
